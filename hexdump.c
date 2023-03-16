a56392d137496f3ea0bcbc9ab83a8359f78445d0
pducos <pducos@student.42.fr>
2023-03-16 01:49:30 +0100

    implement <snip> for duplicate line + optimise

--- hexdump.c
+++ hexdump.c
@@ -6,46 +6,42 @@
 #include <sys/stat.h>
 #include <sys/mman.h>
 
+#define SCREEN_LINE_SIZE 79
+#define BASE "0123456789abcdef"
+
 static char 	*__screen__;
 static int 		__screen_offset__ = 0;
 
-static void __usage_error(char *exec_name)
+static inline void	write_addr(const uintptr_t p)
 {
-	write (STDERR_FILENO, "Error: Usage\n", 14);
-}
-
-inline static void	write_addr(const uintptr_t p)
-{
-	uintptr_t	ptr;
 	char		*buffer = (__screen__ + __screen_offset__);
+	uintptr_t	ptr;
 	int			i;
 
-	i = 16;
+	i = 10;
 	ptr = (uintptr_t)p;
 	do {
-		buffer[--i] = "0123456789abcdef"[ptr & 0xf];
+		buffer[--i] = BASE[ptr & 0xf];
 		ptr >>= 4;
 	} while (ptr && i);
-	memcpy(buffer, "0000000000000000", i);
-	*(int32_t*)(buffer + 15) = 0x20203A;
-	__screen_offset__ += 18;
+	while (i)
+		buffer[i--] = '0';
+	*(int32_t *)(buffer + 10)= 0x20203a;
+	__screen_offset__ += 13;
 }
 
-inline static void	write_data(const void *addr, size_t size)
+static inline void	write_data(const void *addr, size_t size)
 {
-	char	*ptr;
 	char	*buffer = (__screen__ + __screen_offset__);
+	char	*ptr;
 	size_t  i;
 
 	i = 0;
 	ptr = (char *)addr;
 	while (size-- && i < 49) {
 		if (*ptr) {
-			if (*ptr & 0xf0)
-				buffer[i++] = "0123456789abcdef"[(*ptr >> 4) & 0xf];
-			else
-				buffer[i++] = '.';
-			buffer[i++] = "0123456789abcdef"[*ptr & 0xf];
+			buffer[i++] = BASE[(*ptr >> 4) & 0xf];
+			buffer[i++] = BASE[*ptr & 0xf];
 		} else {
 			*(int16_t*)(buffer + i) = 0x2e2e;
 			i += 2;
@@ -58,10 +54,10 @@ inline static void	write_data(const void *addr, size_t size)
 	__screen_offset__ += i;
 }
 
-inline static void	write_ascii(const void *s, size_t size)
+static inline void	write_ascii(const void *s, size_t size)
 {
-	int8_t 	*tmp;
 	char	*buffer = (__screen__ + __screen_offset__);
+	int8_t 	*tmp;
 	size_t	i;
 
 	i = 0;
@@ -71,35 +67,42 @@ inline static void	write_ascii(const void *s, size_t size)
 			(buffer[i++] = *tmp) : (buffer[i++] = '.');
 		tmp++;
 	}
-	buffer[i] = '\n';
-	__screen_offset__ += i + 1;
+	buffer[i++] = '\n';
+	__screen_offset__ += i;
 }
 
-void	print_memory(const void *addr, size_t n)
+static inline void dump_screen(void)
+{
+	write (STDIN_FILENO,
+		__screen__,
+		__screen_offset__);
+	__screen_offset__ = 0;
+}
+
+static void	print_memory(const void *addr, size_t n)
 {
 	size_t		size;
 	const void 	*tmp = addr;
 
 	while (n) {
-		size = n;
-		if (size > 16)
-			size = 16;
-		write_addr(addr - tmp);
-		write_data(addr, size);
-		write_ascii(addr, size);
-        if (__screen_offset__ >= 84 << 4) {
-            write(STDOUT_FILENO, __screen__, 84 << 4);
-            __screen_offset__ = 0;
-        }
+		size = (n > 16 ? 16 : n);
+		if (tmp != addr && !memcmp(addr - 16, addr, 16)) {
+			if (__screen_offset__) {
+				dump_screen();
+				write (STDOUT_FILENO, "<snip>\n", 7);
+			}
+		} else {
+			write_addr(addr - tmp);
+			write_data(addr, size);
+			write_ascii(addr, size);
+        	if (__screen_offset__ >= SCREEN_LINE_SIZE << 4)
+				dump_screen();
+		}
         addr += size;
 		n -= size;
 	}
-    if (__screen_offset__) {
-        write(
-			STDOUT_FILENO,
-			__screen__,
-			__screen_offset__);
-	}
+    if (__screen_offset__)
+		dump_screen();
 }
 
 int main(int ac, char *av[])
@@ -108,11 +111,11 @@ int main(int ac, char *av[])
 	int 		fd;
 	struct stat	st;
 	
-	if (ac < 2)
-		return (
-			__usage_error(av[1]),
-			EXIT_FAILURE);
-	if ((__screen__ = malloc(84 * 16)) == NULL)
+	if (ac < 2) {
+		write (STDERR_FILENO, "Error: Usage\n", 14);
+		return (EXIT_FAILURE);
+	}
+	if ((__screen__ = malloc(SCREEN_LINE_SIZE << 4)) == NULL)
 		return (EXIT_FAILURE);
 	if (stat(av[1], &st) == -1
 		|| (fd = open(av[1], O_RDONLY)) == -1)
