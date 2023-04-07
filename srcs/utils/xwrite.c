cd3d49fcd7bdb68f1363bffac7de89da33a3d484
pducos <pducos@student.42.fr>
2023-04-07 12:48:26 +0200

    improve clarity of xwrite

--- srcs/utils/xwrite.c
+++ srcs/utils/xwrite.c
@@ -1,29 +1,32 @@
 #include <unistd.h>
 #include <errno.h>
+#include <sys/types.h>
 
 /* Forces to write the buffer fully if write() failes, 
  * it's ok if this ends up looping endlessly.
  */
 ssize_t xwrite(int fd, const char *buf, size_t nbytes)
 {
-	int ntry;
 	int i;
-	int n;
+	int n = nbytes;
+	int ntry = 0;
 
-	n = nbytes;
-	ntry = 0;
-	for (;;) {
+	for (;;)
+	{
 		i = write(fd, buf, n);
-		if (i > 0) {
+		if (i > 0)
+		{
 			if ((n -= i) <= 0)
-				return nbytes;
+				return (nbytes);
 			buf += i;
 			ntry = 0;
-		} else if (i == 0) {
+	
+		} else if (i == 0)
+		{
 			if (++ntry > 10)
-				return nbytes - n;
-		} else if (errno != EINTR) {
-			return -1;
-		}
+				return (nbytes - n);
+	
+		} else if (errno != EINTR)
+			return (-1);
 	}
 }
